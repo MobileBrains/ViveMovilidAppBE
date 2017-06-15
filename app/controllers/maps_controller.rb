fa069f4c93d6cde5adc353b69140f9fea42ffa93
Ivan Martinez C <vanmartc@gmail.com>
2017-06-15 02:44:08 -0500

    using ajax to refresh the markers location

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,6 @@
 class MapsController < ApplicationController
+  skip_before_action :authenticate_user!, :only => [:index_map,:updateLocations]
+
   def index_map
     @bus_locations = Vehicle.where.not(latitude: nil, longitude:nil )
      @hash = Gmaps4rails.build_markers(@bus_locations) do |bus_location, marker|
@@ -16,4 +18,27 @@ class MapsController < ApplicationController
                             Numero Lateral: #{bus_location.lateral_number} "
      end
   end
+
+  def updateLocations
+    @bus_locations = Vehicle.where.not(latitude: nil, longitude:nil ).last
+     @hash = Gmaps4rails.build_markers(@bus_locations) do |bus_location, marker|
+        marker.lat bus_location.latitude
+        marker.lng bus_location.longitude
+        marker.picture({
+              "url" => "http://icons.iconarchive.com/icons/icons-land/vista-map-markers/32/Map-Marker-Marker-Outside-Chartreuse-icon.png",
+              "width" => 32,
+              "height" => 32
+            })
+        marker.infowindow "Se scontruye a nuestro gusto<br>
+                            <hr>
+                            mas cosas blablabla <br>
+                            <hr>
+                            Numero Lateral: #{bus_location.lateral_number} "
+     end
+
+    respond_to do |response|
+      response.json { render json: @hash }
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
 
   use_doorkeeper
+  devise_for :user
+
   resources :companies
   resources :bus_routes
   resources :vehicles
-  get 'maps/index_map'
 
   mount RailsAdmin::Engine => '/dashboard', as: 'rails_admin'
   mount API::Base, at: "/viveApi"
 
   root to: "basic#welcome"
 
+  get 'maps/index_map'
   get 'basic/welcome'
 
-  devise_for :users
-
+  post 'maps/updateLocations'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
