f00726576c4e4221e897ea1ca032a74dc67d2cf7
Ivan Martinez C <vanmartc@gmail.com>
2018-10-31 19:02:39 -0500

    testing api on production

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -105,7 +105,7 @@ Doorkeeper.configure do
   #   http://tools.ietf.org/html/rfc6819#section-4.4.2
   #   http://tools.ietf.org/html/rfc6819#section-4.4.3
   #
-  #grant_flows %w(password client_credentials)
+  grant_flows %w(password client_credentials)
 
   # Under some circumstances you might want to have applications auto-approved,
   # so that the user skips the authorization step.
